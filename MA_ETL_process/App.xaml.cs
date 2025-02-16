adeeb8e4af85bead924cb63743dcaccec21996e3
christophKaiser <64251870+christophKaiser@users.noreply.github.com>
2025-02-16 10:07:50 +0100

    Add project files.

---
+++ MA_ETL_process/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MA_ETL_process
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
