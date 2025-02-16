cef2b9601f1cc29c770ffde7248772bd0aa7232b
christophKaiser <64251870+christophKaiser@users.noreply.github.com>
2025-02-16 13:14:00 +0100

    added reference txtConsole in Utilities, added void ConsoleLog

--- MA_ETL_process/Utilities.cs
+++ MA_ETL_process/Utilities.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace MA_ETL_process
 {
     static class Utilities
     {
+        private static TextBox? txtConsole;
+        public static TextBox? SetTxtConsoleReference { set { txtConsole = value; } }
 
+        public static void ConsoleLog(string message)
+        {
+            if (txtConsole != null)
+            {
+                txtConsole.Text += message + "\n";
+                txtConsole.ScrollToEnd();
+            }
+        }
     }
 }
