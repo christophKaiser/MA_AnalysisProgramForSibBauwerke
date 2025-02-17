b2a7d42f2a3b05834837969a0a2be826c367c352
christophKaiser <64251870+christophKaiser@users.noreply.github.com>
2025-02-17 10:24:06 +0100

    implemented SqlClient.SelectRows(..), added call in btn_SqlTest_Click

--- MA_ETL_process/SqlClient.cs
+++ MA_ETL_process/SqlClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +18,40 @@ namespace MA_ETL_process
             _connection.Open();
             Utilities.ConsoleLog("Connection to SQL Server successfully");
             // ToDo: was it realy successful?
+        }
+
+        public void SelectTestRows()
+        {
+            string commandText = @"SELECT TOP (10) * FROM [SIB_BAUWERKE_19_20230427].[dbo].[GES_BW]";
+            SelectRows(commandText);
+        }
+
+        public void SelectRows(string commandText)
+        {
+            using (var command = new SqlC.SqlCommand())
+            {
+                command.Connection = _connection;
+                command.CommandType = CommandType.Text;
+                command.CommandText = commandText;
+
+                SqlC.SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string line = "";
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        //line += reader.GetName(i) + ": " + reader.GetValue(i) + "   ";
+                        line += reader.GetName(i) + ": " + reader[i] + "   ";
+                        // string reader[i].GetDataTypeName()  // fast
+                        // Type reader[i].GetType() // slower than GetDataTypeName()
+                        //                             ... at least on Debug-Console with text output in console
+                    }
+                    Utilities.ConsoleLog(line);
+                }
+
+                reader.Close();
+            }
         }
     }
 }
