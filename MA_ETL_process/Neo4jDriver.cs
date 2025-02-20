bcc46060ec7efb8478c4216b86dad037d2df2303
christophKaiser <64251870+christophKaiser@users.noreply.github.com>
2025-02-20 16:17:42 +0100

    added constructor of class Neo4j to create the neo4j.Driver

--- MA_ETL_process/Neo4jDriver.cs
+++ MA_ETL_process/Neo4jDriver.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Neo4j.Driver;
 
 namespace MA_ETL_process
 {
-    internal class Neo4jDriver
+    internal class Neo4jDriver : IDisposable
     {
+        private readonly IDriver? _driver;
 
+        public Neo4jDriver()
+        {
+            _driver = GraphDatabase.Driver(
+                LoginCredentials.Neo4jUri, 
+                AuthTokens.Basic(LoginCredentials.Neo4jUser, LoginCredentials.Neo4jPassword));
+            Utilities.ConsoleLog("Connection to Neo4j Server successfully");
+            // ToDo: was it realy successful?
+        }
+
+        public void Dispose()
+        {
+            _driver?.Dispose();
+        }
     }
 }
