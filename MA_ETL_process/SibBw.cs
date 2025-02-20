d2ff64a20013f0640af01ed61d089e5401179daf
christophKaiser <64251870+christophKaiser@users.noreply.github.com>
2025-02-20 22:43:08 +0100

    added classes SibBw and SibBw_GES_BW

---
+++ MA_ETL_process/SibBw.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MA_ETL_process
+{
+    internal class SibBw
+    {
+        public Dictionary<string, string> stringValues = new Dictionary<string, string>();
+        public Dictionary<string, double> numberValues = new Dictionary<string, double>();
+    }
+
+    internal class SibBW_GES_BW : SibBw
+    {
+
+    }
+}
