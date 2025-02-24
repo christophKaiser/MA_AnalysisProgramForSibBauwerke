f527b90c6a9d1a3a7f3a82c960e1d7e2fe2bb126
christophKaiser <64251870+christophKaiser@users.noreply.github.com>
2025-02-24 12:00:38 +0100

    added SibBw.GetCypherCreate()

--- MA_ETL_process/SibBw.cs
+++ MA_ETL_process/SibBw.cs
@@ -10,15 +10,48 @@ namespace MA_ETL_process
     {
         public Dictionary<string, string> stringValues = new Dictionary<string, string>();
         public Dictionary<string, double> numberValues = new Dictionary<string, double>();
+
+        protected string GetCypherCreate(string cypherIdentifier, string lable)
+        {
+            // CREATE (a:Person {name:'Brie Larson', born:1989})
+            // initialize CREATE
+            string cypher = $"CREATE ({cypherIdentifier}:{lable} {{";
+
+            // add numberValues as properties
+            foreach (KeyValuePair<string, double> kvp in numberValues)
+            {
+                cypher += $"{kvp.Key}:{kvp.Value}, ";
+            }
+
+            // add stringValues as properties
+            foreach (KeyValuePair<string, string> kvp in stringValues)
+            {
+                cypher += $"{kvp.Key}:'{kvp.Value}', ";
+            }
+
+            // remove last "," in the cypher-string
+            cypher = cypher.Remove(cypher.Length - 2, 1) ;
+
+            // close properties-parenthes and CREATE-parenthesis; return string
+            return cypher += "})";
+        }
     }
 
     internal class SibBW_GES_BW : SibBw
     {
         public List<SibBW_TEIL_BW> teilbauwerke = new List<SibBW_TEIL_BW>();
+
+        public string GetCypherCreate(string cypherIdentifier)
+        {
+            return GetCypherCreate(cypherIdentifier, "GES_BW");
+        }
     }
 
     internal class SibBW_TEIL_BW : SibBw
     {
-
+        public string GetCypherCreate(string cypherIdentifier)
+        {
+            return GetCypherCreate(cypherIdentifier, "TEIL_BW");
+        }
     }
 }
