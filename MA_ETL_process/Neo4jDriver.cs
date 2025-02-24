fb9d523f65d8143e1fe435e1b4ffa7ddf9c93180
christophKaiser <64251870+christophKaiser@users.noreply.github.com>
2025-02-24 16:21:46 +0100

    moved Neo4jDriver Session to be a class-property

--- MA_ETL_process/Neo4jDriver.cs
+++ MA_ETL_process/Neo4jDriver.cs
@@ -9,58 +9,54 @@ namespace MA_ETL_process
 {
     internal class Neo4jDriver : IDisposable
     {
-        private readonly IDriver? _driver;
+        private readonly IDriver _driver;
+        private Neo4j.Driver.ISession _session;
 
         public Neo4jDriver()
         {
             _driver = GraphDatabase.Driver(
                 LoginCredentials.Neo4jUri, 
                 AuthTokens.Basic(LoginCredentials.Neo4jUser, LoginCredentials.Neo4jPassword));
+            _session = _driver.Session(o => o.WithDatabase(LoginCredentials.Neo4jDatabase));
             Utilities.ConsoleLog("Connection to Neo4j Server successfully");
             // ToDo: was it realy successful?
         }
 
         public void PrintGreeting(string message, int iInt, string iString)
         {
-            using (var session = _driver.Session(o => o.WithDatabase(LoginCredentials.Neo4jDatabase)))
-            {
-                var greeting = session.ExecuteWrite(
-                    tx =>
-                    {
-                        // called function: Neo4j.Driver.IQueryRunner.Run(..)
-                        var result = tx.Run(
-                            "CREATE (a:Greeting) " +
-                            //"SET a.message = $message " +
-                            "SET a = {message: $message, iInt: $iInt, iString: $iString} " +
-                            "RETURN a.message + ', from node ' + id(a)",
-                            //new { messsage });
-                            new { message, iInt, iString }); // parameters of the query
-                                                             // test result: iInt is intager in neo4j, iString is string in neo4j
+            var greeting = _session.ExecuteWrite(
+                tx =>
+                {
+                    // called function: Neo4j.Driver.IQueryRunner.Run(..)
+                    var result = tx.Run(
+                        "CREATE (a:Greeting) " +
+                        //"SET a.message = $message " +
+                        "SET a = {message: $message, iInt: $iInt, iString: $iString} " +
+                        "RETURN a.message + ', from node ' + id(a)",
+                        //new { messsage });
+                        new { message, iInt, iString }); // parameters of the query
+                                                            // test result: iInt is intager in neo4j, iString is string in neo4j
 
-                        return result.Single()[0].As<string>();
-                    });
+                    return result.Single()[0].As<string>();
+                });
 
-                Utilities.ConsoleLog(greeting);
-            }
+            Utilities.ConsoleLog(greeting);
         }
 
         private void ExecuteCypherQuery(string query)
         {
-            using (var session = _driver.Session(o => o.WithDatabase(LoginCredentials.Neo4jDatabase)))
-            {
-                var result = session.Run(query);
-                //session.ExecuteWrite(
-                //    tx =>
-                //    {
-                //        // called function: Neo4j.Driver.IQueryRunner.Run(..)
-                //        var result = tx.Run(
-                //            new Query(query));
-                //        return;
-                //        //return result.Single()[0].As<string>();
-                //    });
+            var result = _session.Run(query);
+            //session.ExecuteWrite(
+            //    tx =>
+            //    {
+            //        // called function: Neo4j.Driver.IQueryRunner.Run(..)
+            //        var result = tx.Run(
+            //            new Query(query));
+            //        return;
+            //        //return result.Single()[0].As<string>();
+            //    });
 
-                //Utilities.ConsoleLog(greeting);
-            }
+            //Utilities.ConsoleLog(greeting);
         }
 
         public void DeleteAllInDatabase()
@@ -70,7 +66,8 @@ namespace MA_ETL_process
 
         public void Dispose()
         {
-            _driver?.Dispose();
+            _session.Dispose();
+            _driver.Dispose();
         }
     }
 }
