c341da5228804a04de7b8d9a2902a6e76af6a4e1
christophKaiser <64251870+christophKaiser@users.noreply.github.com>
2025-03-07 07:31:40 +0100

    added SQL- and Cypher-Query for SCHADALT

--- MA_ETL_process/MainWindow.xaml.cs
+++ MA_ETL_process/MainWindow.xaml.cs
@@ -237,13 +237,31 @@ namespace MA_ETL_process
 
             // ---
 
-            // ToDo:
             // SchadenAlt hängt an Prüfung via ID_NR, PRUFJAHR, PRA (=Prüfart: {E, H})
             // aber (!!) noch keine eindeutige identifizierung des Schadens gefunden (LFDNR und SCHAD_ID sind nicht konsistent)
             // vielleicht IDENT nutzen, auch wenn Bedeutung unklar ??
             //SELECT [ID_NR], [LFDNR], [BAUTEIL], [KONTEIL], [ZWGRUPPE], [SCHADEN], [SCHADEN_M], [MENGE_ALL], [MENGE_DI], [MENGE_DI_M], [UEBERBAU], [UEBERBAU_M], [FELD], [FELD_M], [LAENGS], [LAENGS_M], [QUER], [QUER_M], [HOCH], [HOCH_M], [BEWERT_D], [BEWERT_V], [BEWERT_S], [S_VERAEND], [BEMERK1], [BEMERK1_M], [BEMERK2], [BEMERK2_M], [BEMERK3], [BEMERK3_M], [BEMERK4], [BEMERK4_M], [BEMERK5], [BEMERK5_M], [BEMERK6], [BEMERK6_M], [BWNR], [TEIL_BWNR], [IBWNR], [IDENT], [AMT], [PRUFJAHR], [PRA], [TEXT], [BILD], [KONT_JN], [NOT_KONST], [KONVERT], [SCHAD_ID], [BSP_ID], [BAUTLGRUP], [DETAILKONT]
             //FROM[SIB_BAUWERKE_19_20230427].[dbo].[SCHADALT]
             //WHERE[SIB_BAUWERKE_19_20230427].[dbo].[SCHADALT].[BWNR] = 8142509;
+            Utilities.ConsoleLog("\nSchäden Alt");
+            List<SibBW_SCHADFALT> schadAlt_List = sqlClient.SelectRows<SibBW_SCHADFALT>(
+                "SELECT [ID_NR], [LFDNR], [BAUTEIL], [KONTEIL], [ZWGRUPPE], [SCHADEN], [SCHADEN_M], " +
+                "[MENGE_ALL], [MENGE_DI], [MENGE_DI_M], [UEBERBAU], [UEBERBAU_M], [FELD], [FELD_M], [LAENGS], [LAENGS_M], " +
+                "[QUER], [QUER_M], [HOCH], [HOCH_M], [BEWERT_D], [BEWERT_V], [BEWERT_S], [S_VERAEND], [BEMERK1], [BEMERK1_M], " +
+                "[BEMERK2], [BEMERK2_M], [BEMERK3], [BEMERK3_M], [BEMERK4], [BEMERK4_M], [BEMERK5], [BEMERK5_M], " +
+                "[BEMERK6], [BEMERK6_M], [BWNR], [TEIL_BWNR], [IBWNR], [IDENT], [AMT], [PRUFJAHR], [PRA], [TEXT], [BILD], " +
+                "[KONT_JN], [NOT_KONST], [KONVERT], [SCHAD_ID], [BSP_ID], [BAUTLGRUP], [DETAILKONT]" +
+                "FROM[SIB_BAUWERKE_19_20230427].[dbo].[SCHADALT]" +
+                "WHERE[SIB_BAUWERKE_19_20230427].[dbo].[SCHADALT].[BWNR]" +
+                $"IN('{String.Join("', '", bridgeNumbers)}')");
+
+            foreach (SibBW_SCHADFALT schadAlt in schadAlt_List)
+            {
+                query += schadAlt.GetCypherCreate() + "\n";
+            }
+            neo4jDriver.ExecuteCypherQuery(query);
+            query = "";
+            teilbauwerke.Clear();
 
             // ---
 
